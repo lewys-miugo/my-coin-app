61442331bff6b5e643bb5cf3f8d4932bbb8e7ff4
ogzacky75 <ogzacky75@gmail.com>
2025-07-23 12:42:51 +0300

    coin counter

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,3 +1,6 @@
+import { data } from "autoprefixer"
+import { useEffect,useState } from "react"
+
 const URL = "https://api.coinlore.net/api/tickers/"
 
 function Dashboard (){
@@ -20,18 +23,38 @@ function Dashboard (){
 }
 
 function TotalCoins(){
+
+	const [coins, setCoins]= useState([])
+
+	
+	useEffect(()=>{
+		fetch (URL)
+		.then(res=>res.json())
+		.then(data=>{
+
+			console.log(data.length);
+				setCoins(data.data);
+		})
+		.catch(error=>{
+			console.error('error')
+		})
+	}, []);
+
+
 	return (
-		<div className="bg-black text-green-500 w-1/3 rounded-[10px] mt-[20px] ml-[10px]">
+		<div className="bg-black text-green-500 w-1/3 rounded-[10px] mt-[20px] ml-[10px] text-center">
 
 			<h2 className="">Total Number of coins</h2>
-
+			<p>
+				{coins.length}
+			</p>
 		</div>
 	)
 }
 
 function StaredCoins(){
 	return (
-		<div className="bg-black text-blue-500 w-1/3 rounded-[10px] mt-[20px]">
+		<div className="bg-black text-blue-500 w-1/3 rounded-[10px] mt-[20px] text-center">
 
 			<h2>Coins Stared</h2>
 
@@ -41,7 +64,7 @@ function StaredCoins(){
 
 function CreatedCoins(){
 	return (
-		<div className="bg-black text-red-500 w-1/3 rounded-[10px] mt-[20px] mr-[10px]">
+		<div className="bg-black text-red-500 w-1/3 rounded-[10px] mt-[20px] mr-[10px] text-center">
 
 			<h2>Coins Created</h2>
 
