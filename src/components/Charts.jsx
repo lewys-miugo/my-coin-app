8b90c862879731c8355ee025aa8534fcf3e458ec
ogzacky75 <ogzacky75@gmail.com>
2025-07-24 11:05:32 +0300

    adding the chart components

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,11 +1,55 @@
 'use client';
+import { useState, useEffect } from "react";
 
-import { AreaChart, Area } from "recharts";
+import { data } from "autoprefixer";
+import { AreaChart, Area, ResponsiveContainer, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import { type } from "@testing-library/user-event/dist/type";
 
-function ChartComponent(){
+function Chart(){
+	const [products, setProducts] = useState([])
+
+
+  	useEffect(() => {
+    	fetch("https://api.coinlore.net/api/tickers/")
+      	.then(res => res.json())
+      	.then(data => {
+        	setProducts(data.data); 
+      	})
+      	.catch(err => console.error("Failed to fetch data:", err));
+  	}, 	[]);
+
 	return (
+
+		<div className="h-[60vh] w-[500px]">
+			<ResponsiveContainer width="100%" height="100%">
+
+				<AreaChart width={500} height={400} data={products}>
+
+					<XAxis></XAxis>
+					<YAxis></YAxis>
+					<CartesianGrid></CartesianGrid>
+					
+					
+					<Area dataKey="percent_change_24h" type="monotone" stroke="green" fill="transparent"></Area>
+					<Area dataKey="percent_change_1h" type="monotone" stroke="blue" fill="transparent"></Area>
+					<Area dataKey="percent_change_7d" type="monotone" stroke="red" fill="transparent"></Area>
+			
+			
+				</AreaChart>
+
+
+			</ResponsiveContainer>
+
+			
+
+
+		
+
+		</div>
+
+		
 		
 	)
 };
 
-export default ChartComponent;
+export default Chart;
